850d81dbd667a92db2b5c3780c5783581408fa82
Karmanyaah Malhotra <karmanyaahm@gmail.com>
2021-01-06 19:32:34 -0500

    style

--- src/Table.tsx
+++ src/Table.tsx
@@ -288,31 +288,28 @@ function Tablee({ columns, data }) {
                 <button onClick={() => previousPage()} disabled={!canPreviousPage}>
                     {'<'}
                 </button>{' '}
+<span>
+                    Page{' '}
+                    <strong>
+                             <input
+                        type="number"
+                        onChange={e => {
+                            const page = e.target.value ? Number(e.target.value) - 1 : 0
+                            gotoPage(page)
+                        }}
+			value={(pageIndex+1).toString()}
+                        style={{ width: '3em', }}
+                    /> of {pageOptions.length}
+                    </strong>{' '}
+                </span>
+
                 <button onClick={() => nextPage()} disabled={!canNextPage}>
                     {'>'}
                 </button>{' '}
                 <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
                     {'>>'}
                 </button>{' '}
-                <span>
-                    Page{' '}
-                    <strong>
-                        {pageIndex + 1} of {pageOptions.length}
-                    </strong>{' '}
-                </span>
-                <span>
-                    | Go to page:{' '}
-                    <input
-                        type="number"
-                        defaultValue={pageIndex + 1}
-                        onChange={e => {
-                            const page = e.target.value ? Number(e.target.value) - 1 : 0
-                            gotoPage(page)
-                        }}
-                        style={{ width: '100px' }}
-                    />
-                </span>{' '}
-                <select
+                                <select
                     value={pageSize}
                     onChange={e => {
                         setPageSize(Number(e.target.value))
